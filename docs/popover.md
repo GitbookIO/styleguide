4ac5c085f8c4b7e4602dce0c9c2045fdc5013af2
Samy Pesse <samypesse@gmail.com>
2016-07-22 18:51:43 +0200

    Start style for popovers

---
+++ docs/popover.md
@@ -0,0 +1,21 @@
+---
+layout: default
+title: Popovers
+order: 10
+---
+
+# Popovers
+
+Add small overlay content, like those found in iOS, to any element for housing secondary information.
+
+{% example html %}
+<div class="popover">
+    <div class="popover-arrow"></div>
+    <div class="popover-heading">
+        <h4>Title</h4>
+    </div>
+    <div class="popover-body">
+        Body of the modal
+    </div>
+</div>
+{% endexample %}
---
+++ src/Popover.js
@@ -0,0 +1,63 @@
+var React = require('react');
+var classNames = require('classnames');
+
+const POSITIONS = ['bottom', 'top', 'left', 'right'];
+
+var Popover =  React.createClass({
+    propTypes: {
+        position: React.PropTypes.oneOf(POSITIONS)
+    },
+
+    getDefaultProps: function() {
+        return {
+            position: POSITIONS[0]
+        };
+    },
+
+    render: function() {
+        var position  = this.props.position;
+        var className = classNames('popover', 'popover-' + position);
+
+        return (
+            <div className={className}>
+                {this.props.children}
+            </div>
+        );
+    }
+});
+
+var PopoverBody =  React.createClass({
+    render: function() {
+        return (
+            <div className="popover-body">
+                {this.props.children}
+            </div>
+        );
+    }
+});
+
+var PopoverHeading =  React.createClass({
+    propTypes: {
+        title: React.PropTypes.string
+    },
+
+    render: function() {
+        if (this.props.title) {
+            return (
+                <div className="popover-heading">
+                    <h4>{this.props.title}</h4>
+                </div>
+            );
+        }
+
+        return (
+            <div className="popover-header">
+                {this.props.children}
+            </div>
+        );
+    }
+});
+
+module.exports         = Popover;
+module.exports.Body    = PopoverBody;
+module.exports.Heading = PopoverHeading;
