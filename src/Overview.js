ce0afa4ec8ae441da9c317e6d8576c87588bb4f8
Soreine <nicolas@gitbook.com>
2016-07-29 11:01:41 +0200

    Use React.createClass

--- src/Overview.js
+++ src/Overview.js
@@ -4,23 +4,32 @@ var React = require('react');
  * Component to create overviews/intros with a title, description,
  * metas info (links), and a picture.
  */
-var Overview = function (props) {
-    return <div className="overview">{ this.props.children }</div>;
-};
+var Overview = React.createClass({
+    render: function () {
+        return <div className="overview">{ this.props.children }</div>;
+    }
+});
 
-var Title = function (props) {
-    return <h1 className="overview-title">{ this.props.children }</h1>;
-};
+Overview.Title = React.createClass({
+    render: function () {
+        return <h1 className="overview-title">{ this.props.children }</h1>;
+    }
+});
 
-var Description = function (props) {
-    return <p className="overview-description">{ this.props.children }</p>;
-};
+Overview.Description = React.createClass({
+    render: function () {
+        return <p className="overview-description">{ this.props.children }</p>;
+    }
+});
 
-var Meta = function (props) {
-    return <div className="overview-meta">{ this.props.children }</div>;
-};
+Overview.Meta = React.createClass({
+    render: function () {
+        return <div className="overview-meta">{ this.props.children }</div>;
+    }
+});
 
 module.exports = Overview;
-module.exports.Title = Title;
-module.exports.Description = Description;
-module.exports.Meta = Meta;
+// Overview
+// Overview.Title
+// Overview.Description
+// Overview.Meta
