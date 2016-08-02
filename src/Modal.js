68cac366583b0603f6eafc9cd30b204819223176
Samy Pessé <samypesse@gmail.com>
2016-08-02 15:32:51 +0200

    Add component for modal backdrop

--- src/Modal.js
+++ src/Modal.js
@@ -1,9 +1,9 @@
-var React = require('react');
-var classNames = require('classnames');
+const React = require('react');
+const classNames = require('classnames');
 
-var SIZES = require('./SIZES');
+const SIZES = require('./SIZES');
 
-var Modal = React.createClass({
+const Modal = React.createClass({
     propTypes: {
         size:     React.PropTypes.oneOf(SIZES),
         backdrop: React.PropTypes.bool,
@@ -19,46 +19,67 @@ var Modal = React.createClass({
     },
 
     render: function() {
-        var className = classNames('modal',
+        let className = classNames('modal',
             'modal-' + this.props.size,
             this.props.className, {
                 'without-backdrop': !this.props.backdrop
             });
 
-        return <div className={className}>
-            {this.props.children}
-        </div>;
+        return (
+            <div className={className}>
+                {this.props.children}
+            </div>
+        );
     }
 });
 
-var ModalBody = React.createClass({
+const ModalBackdrop = React.createClass({
     propTypes: {
         children: React.PropTypes.node
     },
 
     render: function() {
-        return <div className="modal-body">
-            {this.props.children}
-        </div>;
+        return (
+            <div className="modal-backdrop ">
+                {this.props.children}
+            </div>
+        );
     }
 });
 
-var ModalFooter = React.createClass({
+const ModalBody = React.createClass({
     propTypes: {
         children: React.PropTypes.node
     },
 
     render: function() {
-        return <div className="modal-footer ">
-            {this.props.children}
-        </div>;
+        return (
+            <div className="modal-body">
+                {this.props.children}
+            </div>
+        );
     }
 });
 
-var ModalHeading = React.createClass({
+const ModalFooter = React.createClass({
+    propTypes: {
+        children: React.PropTypes.node
+    },
+
+    render: function() {
+        return (
+            <div className="modal-footer ">
+                {this.props.children}
+            </div>
+        );
+    }
+});
+
+const ModalHeading = React.createClass({
     propTypes: {
         children: React.PropTypes.node,
-        onClose: React.PropTypes.func
+        onClose:  React.PropTypes.func,
+        title:    React.PropTypes.string
     },
 
     onClose: function(e) {
@@ -68,17 +89,24 @@ var ModalHeading = React.createClass({
 
     render: function() {
         if (this.props.title) {
-            return <div className="modal-heading">
-                <h4>{this.props.title}</h4>
-                {this.props.onClose? <a href="#" onClick={this.onClose} className="modal-close">&times;</a> : ''}
-            </div>;
+            return (
+                <div className="modal-heading">
+                    <h4>{this.props.title}</h4>
+                    {this.props.onClose? <a href="#" onClick={this.onClose} className="modal-close">&times;</a> : ''}
+                </div>
+            );
         }
 
-        return <div className="modal-heading">{this.props.children}</div>;
+        return (
+            <div className="modal-heading">
+                {this.props.children}
+            </div>
+        );
     }
 });
 
-module.exports         = Modal;
-module.exports.Body    = ModalBody;
-module.exports.Footer  = ModalFooter;
-module.exports.Heading = ModalHeading;
+module.exports          = Modal;
+module.exports.Backdrop = ModalBackdrop;
+module.exports.Body     = ModalBody;
+module.exports.Footer   = ModalFooter;
+module.exports.Heading  = ModalHeading;
