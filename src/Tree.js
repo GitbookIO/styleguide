10a907ebec0a01348b285ed6f5685e7b3e23e3e3
Samy Pessé <samypesse@gmail.com>
2016-08-04 14:23:38 +0200

    Add component for tree

---
+++ src/Tree.js
@@ -0,0 +1,156 @@
+const React = require('react');
+const classNames = require('classnames');
+
+const Icon = require('./icon');
+
+const PADDING_INITIAL = 10;
+const DEPTH_PADDING   = 18;
+
+const STATUS = [
+    'danger', 'success', 'warning', 'selected'
+];
+
+/**
+ * Tree component:
+ *
+ * <Tree>
+ *      <Tree.Node>
+ *          <Tree.Leaf>
+ *              <Tree.Toggle /> My Directory
+ *          </Tree.Leaf>
+ *          <Tree>...</Tree>
+ *      </Tree.Node>
+ *      <Tree.Node>
+ *          <Tree.Leaf>My File</Tree.Leaf>
+ *      </Tree.Node>
+ * </Tree>
+ */
+
+const Tree = React.createClass({
+    propTypes: {
+        children: React.PropTypes.node,
+        depth: React.PropTypes.number
+    },
+    contextTypes: {
+        depth: React.PropTypes.number
+    },
+    childContextTypes: {
+        depth: React.PropTypes.number
+    },
+
+    getChildContext: function() {
+        let depth = this.props.depth || this.context.depth || 0;
+
+        return {
+            depth: (depth + 1)
+        };
+    },
+
+    render: function() {
+        return (
+            <div className="Tree">
+                {this.props.children}
+            </div>
+        );
+    }
+});
+
+const TreeNode = React.createClass({
+    propTypes: {
+        children: React.PropTypes.node
+    },
+
+    render: function() {
+        return (
+            <div className="TreeNode">
+                {this.props.children}
+            </div>
+        );
+    }
+});
+
+/**
+ * Inner content of a tree node
+ */
+const TreeLeaf = React.createClass({
+    propTypes: {
+        children: React.PropTypes.node,
+        href:     React.PropTypes.string,
+        onClick:  React.PropTypes.func,
+        status:   React.PropTypes.oneOf(STATUS)
+    },
+    contextTypes: {
+        depth: React.PropTypes.number
+    },
+
+    getDefaultProps: function() {
+        return {
+            href: '#'
+        };
+    },
+
+    onClick: function(event) {
+        let { onClick } = this.props;
+        if (!onClick) {
+            return;
+        }
+
+        event.preventDefault();
+        onClick();
+    },
+
+    render: function() {
+        let { href, status } = this.props;
+        let { depth } = this.context;
+
+        let style = {
+            paddingLeft: (PADDING_INITIAL + depth*DEPTH_PADDING)
+        };
+
+        let className = classNames('TreeLeaf', status? 'status-' + status : '');
+
+        return (
+            <a className={className} href={href} style={style}>
+                {this.props.children}
+            </a>
+        );
+    }
+});
+
+/**
+ * Little triangle to toggle sub-tree.
+ * Insert a TreeToggle in a TreeLeaf
+ */
+const TreeToggle = React.createClass({
+    propTypes: {
+        active:  React.PropTypes.bool,
+        onClick: React.PropTypes.func
+    },
+
+    onClick: function(event) {
+        let { onClick } = this.props;
+
+        event.preventDefault();
+        event.stopPropagation();
+
+        if (onClick) onClick();
+    },
+
+    render: function() {
+        let { active } = this.props;
+        let className = classNames('TreeToggle', {
+            active: active
+        });
+
+        return (
+            <span className={className}>
+                <Icon id={active? 'triangle-down' : 'triangle-right'} />
+            </span>
+        );
+    }
+});
+
+module.exports = Tree;
+module.exports.Node = TreeNode;
+module.exports.Leaf = TreeLeaf;
+module.exports.Toggle = TreeToggle;
