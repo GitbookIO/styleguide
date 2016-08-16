7a71d1024c8665133c64a8ea36476ab682a506fc
Soreine <nicolas@gitbook.com>
2016-08-16 10:46:31 +0200

    Add proper ProgressBar animation

--- src/ProgressBar.js
+++ src/ProgressBar.js
@@ -3,41 +3,99 @@ const { bool, number } = React.PropTypes;
 
 /**
  * Displays a progress bar (YouTube-like) at the top of container
+ * Based on https://github.com/lonelyclick/react-loading-bar/blob/master/src/Loading.jsx
  */
 const ProgressBar = React.createClass({
     propTypes: {
-        show:  bool,
-        // 0 < percent < 1
-        percent: number
+        show:  bool
     },
 
     getDefaultProps: function () {
         return {
-            show: false,
-            percent: 0.3
+            show: false
         };
     },
 
     getInitialState: function () {
         return {
-            percent: 0
+            size: 0,
+            disappearDelayHide: false, // when dispappear, first transition then display none
+            percent: 0,
+            appearDelayWidth: 0 // when appear, first display block then transition width
         };
     },
 
+    componentWillReceiveProps: function (nextProps) {
+        const { show } = nextProps;
+
+        if (show) {
+            this.show();
+        } else {
+            this.hide();
+        }
+    },
+
+    shouldComponentUpdate: function (nextProps, nextState) {
+        return true; // !shallowEqual(nextState, this.state)
+    },
+
+    show: function () {
+        let { size, percent } = this.state;
+
+        const appearDelayWidth = size === 0;
+        percent = calculatePercent(percent);
+
+        this.setState({
+            size: ++size,
+            appearDelayWidth,
+            percent
+        });
+
+        if (appearDelayWidth) {
+            setTimeout(() => {
+                this.setState({
+                    appearDelayWidth: false
+                });
+            });
+        }
+    },
+
+    hide: function () {
+        let { size } = this.state;
+
+        if (--size < 0) {
+            this.setState({ size: 0 });
+            return;
+        }
+
+        this.setState({
+            size: 0,
+            disappearDelayHide: true,
+            percent: 1
+        });
+
+        setTimeout(() => {
+            this.setState({
+                disappearDelayHide: false,
+                percent: 0
+            });
+        }, 500);
+    },
+
     getBarStyle: function () {
-        const { show, percent } = this.props;
+        const { disappearDelayHide, appearDelayWidth, percent } = this.state;
 
         return {
-            width: percent * 100 + '%',
-            display: show && percent > 0 ? 'block': 'none'
+            width: appearDelayWidth ? 0 : percent * 100 + '%',
+            display: disappearDelayHide || percent > 0 ? 'block': 'none'
         };
     },
 
     getShadowStyle: function () {
-        const { show, percent } = this.props;
+        const { percent, disappearDelayHide } = this.state;
 
         return {
-            display: show && percent > 0 ? 'block': 'none'
+            display: disappearDelayHide || percent > 0 ? 'block': 'none'
         };
     },
 
@@ -51,5 +109,16 @@ const ProgressBar = React.createClass({
         </div>;
     }
 });
+
+function calculatePercent(percent) {
+    percent = percent || 0;
+
+    // How much of remaining bar we advance
+    let progress = 0.1 + Math.random() * 0.2;
+
+    return percent +  progress * (1 - percent);
+}
+
+
 
 module.exports = ProgressBar;
