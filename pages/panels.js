abf77ad4062a3fb5b659c679fb16dd4988469f69
Samy Pessé <samypesse@gmail.com>
2016-11-16 12:54:30 +0100

    Add doc for panels

--- pages/panels.js
+++ pages/panels.js
@@ -5,8 +5,10 @@ const Example = require('./components/Example');
 const CodeEditor = require('./components/CodeEditor');
 
 const Panel = require('../src/Panel');
+const Table = require('../src/Table');
+const ListGroup = require('../src/ListGroup');
 
-const SCOPE = { React, Panel };
+const SCOPE = { React, Panel, Table, ListGroup };
 
 const EXAMPLE_IMPORT =
 'const Panel = require(\'gitbook-styleguide/lib/Panel\');';
@@ -19,6 +21,38 @@ const EXAMPLE_DEFAULT =
     </Panel.Body>
 </Panel>`;
 
+const EXAMPLE_LISTGROUP =
+`<Panel>
+    <Panel.Heading title="Title" />
+    <ListGroup>
+        <ListGroup.Item active={true}>Account</ListGroup.Item>
+        <ListGroup.Item>Profile</ListGroup.Item>
+        <ListGroup.Item>Notifications</ListGroup.Item>
+    </ListGroup>
+</Panel>`;
+
+const EXAMPLE_TABLE =
+`<Panel>
+    <Panel.Heading title="Title" />
+    <Table>
+        <thead>
+            <tr>
+                <th>Title</th>
+                <th>Updated</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>PHP</td>
+                <td>1 month ago</td>
+            </tr>
+            <tr>
+                <td>JS</td>
+                <td>1 month ago</td>
+            </tr>
+        </tbody>
+    </Table>
+</Panel>`;
 
 export default () => {
     return (
@@ -34,6 +68,14 @@ export default () => {
             </Panel>
 
             <Example title="Example" source={EXAMPLE_DEFAULT} scope={SCOPE}>
+
+            </Example>
+
+            <Example title="With a table" source={EXAMPLE_TABLE} scope={SCOPE}>
+
+            </Example>
+
+            <Example title="With a list group" source={EXAMPLE_LISTGROUP} scope={SCOPE}>
 
             </Example>
         </Page>
